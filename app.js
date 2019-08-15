b7a2bf7947a09c0972e60c7d5eb405dc001810d1
Kyle <chiefmccain85@gmail.com>
2019-08-15 09:41:26 -0600

    Added dotenv and MongoDB

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
+// Add environmental variables
+require('dotenv').config();
+
+// Setup express variables
 const express = require('express');
 const app = express();
 const port = 3000;
 
+// // Connect to MongoDB
+// const mongoose = require('mongoose');
+// mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true});
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'))
+// db.once('open', () => {
+//     console.log('Successfully connected to MongoDB!');
+// });
+
+// Allow express to parse incoming JSON files
+app.use(express.json());
+
 // Add routes
-const testRouter = require('./routes/test')
-const issueRouter = require('./routes/issue')
-const questionRouter = require('./routes/question')
+const testRouter = require('./routes/test');
+const issueRouter = require('./routes/issue');
+const questionRouter = require('./routes/question');
 
 // Set test route
 app.use('/test', testRouter);
