404530a64f8c81b344f4e7a40b9d7c3b2490ed5e
Kyle <chiefmccain85@gmail.com>
2019-08-15 15:33:00 -0600

    Added post request placeholder

--- routes/issue.js
+++ routes/issue.js
@@ -5,4 +5,9 @@ router.get('/', (req, res) => {
     res.send('<h>Issue Submission Page</h>');
 });
 
+router.post('/', (req, res) => {
+    console.log(req.body);
+    res.send("Thank you for your issue submission!")
+})
+
 module.exports = router;
