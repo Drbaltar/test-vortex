e8ae13bb8d21281ff65a7a2cb30da15be9549e16
Drbaltar <chiefmccain85@gmail.com>
2019-08-16 06:03:56 -0600

    Added new post request

--- routes/issue.js
+++ routes/issue.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const Issue = require('../models/issue');
 
 router.get('/', (req, res) => {
     res.send('<h>Issue Submission Page</h>');
 });
 
 router.post('/', (req, res) => {
-    console.log(req.body);
+    let newIssue =  new Issue({
+        question_id: req.body.questionID,
+        issue_type: req.body.issueType,
+        issue_description: req.body.issueDescription
+    });
+
+    newIssue.save().then(function(result) {console.log(result)}, 
+        () => console.log("FAILED!"));
+    // console.log(savedIssue === newIssue);
+    
     res.send("Thank you for your issue submission!")
 })
 
