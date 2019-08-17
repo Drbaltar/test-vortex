cb04c3d72ae5668a240483cf1c1abb32d2fdad0f
Drbaltar <chiefmccain85@gmail.com>
2019-08-17 11:14:03 -0600

    Added validators to schema

--- models/issue.js
+++ models/issue.js
@@ -2,9 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const issueSchema = new Schema({
-    question_id: Number,
-    issue_type: String,
-    issue_description: String
+    question_id: {
+        type: Number,
+        required: "The 'Question ID' field is required"
+    },
+    issue_type: {
+        type: String,
+        required: "The 'Issue Type' field is required"
+    },
+    issue_description: {
+        type: String,
+        required: "The 'Issue Description' field is required",
+        minlength: 3
+    }
 });
 
 const Issue = mongoose.model('Issue', issueSchema);
