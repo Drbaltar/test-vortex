c8ed3b9b7714b421011ceb59eccbc6e3e5595d16
Drbaltar <chiefmccain85@gmail.com>
2019-08-18 21:08:16 -0600

    Added ability to insert unique issue number

--- routes/issue.js
+++ routes/issue.js
@@ -1,27 +1,51 @@
 const express = require('express');
 const router = express.Router();
 const Issue = require('../models/issue');
+const Counter = require('../models/counter')
 
 router.get('/', (req, res) => {
     res.send('<h>Issue Submission Page</h>');
 });
 
 router.post('/', (req, res) => {
+    // Create a new Issue document with the input data
     let newIssue =  new Issue({
+        issue_number: 0,
         question_id: req.body.questionID,
         issue_type: req.body.issueType,
         issue_description: req.body.issueDescription
     });
-    
-    newIssue.save()
-        .then((result) => {
-            console.log(result);
-            res.send('Thank you for your issue submission!');
-        })
-        .catch((error) => {
-            console.log('Error: ' + error);
-            res.send(error);
-        });
+
+    // Validate document requirements before going any further
+    newIssue.validate((err) => {
+        if (err) {
+            console.log("Validation Failed: " + err);
+            res.send(err);
+        } else {
+            // Retrieve and increment issue counter from database
+            Counter.findOneAndUpdate({counter_type: "issue"}, { $inc: {sequence_value: 1}},
+            (err, issueCounter) => {
+                if (err) {
+                    console.log('Error: ' + err);
+                    res.send(err);
+                } else {
+                    // Assign issue id to new issue
+                    newIssue.issue_number = issueCounter.sequence_value,
+                    
+                    // Save new issue document to the database
+                    newIssue.save()
+                        .then((result) => {
+                            console.log(result);
+                            res.send('Thank you for your issue submission!');
+                        })
+                        .catch((err) => {
+                            console.log('Error: ' + err);
+                            res.send(err);
+                        });
+                }
+            });
+        }
+    });
 });
 
 module.exports = router;
