74b7b5c1593014072250302b7d1cf03ad22109a5
Kyle <chiefmccain85@gmail.com>
2019-08-21 12:34:42 -0600

    Initial commit

---
+++ src/document-builder.js
@@ -0,0 +1,38 @@
+const Issue = require('../models/issue');
+const MultQuestion = require('../models/patriot/mult-question');
+
+const buildIssueDocument = (req) => {
+    return new Issue({
+        question_id: req.body.questionID,
+        issue_type: req.body.issueType,
+        issue_description: req.body.issueDescription
+    });
+};
+
+const buildMultQuestionDocument = (status, req) => {
+    let inputData = {
+        question_type: req.body.questionType,
+        question_description: req.body.questionDescription,
+        answer_a: req.body.answerA,
+        answer_b: req.body.answerB,
+        answer_c: req.body.answerC,
+        correct_answer: req.body.correctAnswer,
+        gunnery_table: req.body.gunneryTable,
+        test_type: req.body.testType,
+        topic: req.body.topic,
+        reference: req.body.reference
+    };
+
+    if (status === 'pending') {
+        return new MultQuestion.PendingMultQuestion(inputData); 
+    } else if (status === 'approved') {
+        return new MultQuestion.MultQuestion(inputData); 
+    } else {
+        throw new Error('The input status must be either \'pending\' or \'approved\'.');
+    }
+};
+
+module.exports = {
+    buildIssueDocument,
+    buildMultQuestionDocument
+};
