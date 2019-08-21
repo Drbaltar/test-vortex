82c60fdb31798f1ead8fbd4dcdbc5dcf186f4e71
Kyle <chiefmccain85@gmail.com>
2019-08-21 12:32:22 -0600

    Added question_number field and required messages

--- models/patriot/mult-question.js
+++ models/patriot/mult-question.js
@@ -1,52 +1,52 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// Schema for a multiple choice question
 const multQuestionSchema = new Schema({
+    question_number: Number,
     question_type: {
         type: String,
-        required: true,
+        required: 'The \'Question Type\' field is required',
         default: 'Multiple Choice'
     },
     question_description: {
         type: String,
-        required: true,
+        required: 'The \'Question Description\' field is required',
         minlength: 10
     },
     answer_a: {
         type: String,
-        required: true
+        required: 'The \'Answer A\' field is required'
     },
     answer_b: {
         type: String,
-        required: true
+        required: 'The \'Answer B\' field is required'
     },
     answer_c: {
         type: String,
-        required: true
+        required: 'The \'Answer C\' field is required'
     },
     correct_answer: {
         type: String,
-        required: true
+        required: 'The \'Correct Answer\' field is required'
     },
     gunnery_table: {
         table: {
             type: String,
-            required: true
+            required: 'The \'Gunnery Table\' field is required'
         },
         subtask: {
             type: Number,
-            required: true
+            required: 'The \'Gunnery Table Subtask\' field is required'
         }
     },
     test_type: {
         type: String,
         enum: ['Tactics', 'Communications', 'RSOP', 'Early Warning/Mission Command', 'Launcher', 'Tactics/Communications'],
-        required: true
+        required: 'The \'Test Type\' field is required'
     },
     topic: {
         type: String,
-        required: true
+        required: 'The \'Topic\' field is required'
     },
     reference: String
 });
