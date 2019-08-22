9ef162e033af62d4bdeaa9f78f308fd63d826957
Kyle <chiefmccain85@gmail.com>
2019-08-22 12:03:57 -0600

    Added 'True or False' questions

--- src/document-builder.js
+++ src/document-builder.js
@@ -1,5 +1,6 @@
 const Issue = require('../models/issue');
 const MultQuestion = require('../models/patriot/mult-question');
+const TFQuestion = require('../models/patriot/tf-question');
 
 const buildIssueDocument = (req) => {
     return new Issue({
@@ -32,7 +33,31 @@ const buildMultQuestionDocument = (status, req) => {
     }
 };
 
+const buildTFQuestionDocument = (status, req) => {
+    let inputData = {
+        question_type: req.body.questionType,
+        question_description: req.body.questionDescription,
+        answer_a: req.body.answerA,
+        answer_b: req.body.answerB,
+        answer_c: req.body.answerC,
+        correct_answer: req.body.correctAnswer,
+        gunnery_table: req.body.gunneryTable,
+        test_type: req.body.testType,
+        topic: req.body.topic,
+        reference: req.body.reference
+    };
+
+    if (status === 'pending') {
+        return new TFQuestion.PendingTFQuestion(inputData); 
+    } else if (status === 'approved') {
+        return new TFQuestion.TFQuestion(inputData); 
+    } else {
+        throw new Error('The input status must be either \'pending\' or \'approved\'.');
+    }
+};
+
 module.exports = {
     buildIssueDocument,
-    buildMultQuestionDocument
+    buildMultQuestionDocument,
+    buildTFQuestionDocument
 };
