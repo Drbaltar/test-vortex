085a8a67e5a7760a2d35912875547293ff737631
Kyle <chiefmccain85@gmail.com>
2019-08-28 08:12:53 -0600

    Removed NewQuestionInput from jumbotron

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -17,8 +17,8 @@ class App extends React.Component {
             <h1 >Test Vortex</h1>
             <hr/>
             <h4>A collaborative web application used to generate standardized tests!</h4>
+          </div>
             <NewQuestionInputForm/>
-          </div> 
       </div> 
     );
   }
