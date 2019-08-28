f4d622cc4ca85ea27c1bcbb2cf17cf4c0077cd9f
Kyle <chiefmccain85@gmail.com>
2019-08-28 08:15:20 -0600

    Changed 'for' to 'htmlFor'

--- client/src/App/NewQuestion/TextArea.js
+++ client/src/App/NewQuestion/TextArea.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TextField = (props) => {
     return(
         <div className="form-group">
-            <label for={props.id}>{props.label}</label>
+            <label htmlFor={props.id}>{props.label}</label>
             <textarea className="form-control" type={props.type} id={props.id}  value={props.value}
                 placeholder={"Please enter the " + props.label.toLowerCase()} rows={props.rows}
                 onChange={props.inputChange}
