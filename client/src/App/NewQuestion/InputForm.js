c438eb7aba09890d3754ce8d8821e02335fe6dfd
Kyle <chiefmccain85@gmail.com>
2019-08-28 17:31:22 -0600

    Added more fields and fetch request

--- client/src/App/NewQuestion/InputForm.js
+++ client/src/App/NewQuestion/InputForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Axios from 'axios'
 import TextField from './Components/TextField';
 import TextArea from './Components/TextArea';
 import SelectBox from './Components/SelectBox';
@@ -15,7 +16,11 @@ class InputForm extends React.Component {
             correctAnswer: '',
             answerA: '',
             answerB: '',
-            answerC: ''
+            answerC: '',
+            testType: 'Tactics',
+            topic: '',
+            table: '', 
+            subtask: ''
         }
 
         this.state = this.initialState;
@@ -30,6 +35,19 @@ class InputForm extends React.Component {
         event.preventDefault();
         if (event.target.id === 'clearAllButton') {
             this.setState(this.initialState);
+        } else if (event.target.id === 'submitButton') {
+            fetch('/api/questions/submit', {
+                    method: 'POST',
+                    body: this.state
+            })
+            .then(response => console.log(response))
+
+            // const response = await Axios.post(
+            //     'http://localhost:5000/api/questions/submit'
+            //     );
+
+            // console.log(response);
+            
         }
     }
 
@@ -53,6 +71,7 @@ class InputForm extends React.Component {
     };
 
     render() {
+        // Render additional answer fields if question is Multiple Choice
         let multChoiceAnswers = this.checkMultipleChoice();
 
         return(
@@ -70,6 +89,19 @@ class InputForm extends React.Component {
                             value={this.state.correctAnswer}
                             inputChange={(event) => this.handleInputChange(event)}/>
                         {multChoiceAnswers}
+                        <SelectBox label="Test Type" id="testType"
+                            options={['Tactics', 'Communications', 'RSOP', 'Early Warning/Mission Command', 'Launcher', 'Tactics/Communications']}
+                            value={this.state.testType}
+                            inputChange={(event) => this.handleInputChange(event)}/>
+                        <TextField label="Topic"id="topic" type="text"
+                            value={this.state.topic}
+                            inputChange={(event) => this.handleInputChange(event)}/>
+                        <TextField label="Gunnery Table"id="table" type="text"
+                            value={this.state.table}
+                            inputChange={(event) => this.handleInputChange(event)}/>
+                        <TextField label="Table Subtask"id="subtask" type="text"
+                            value={this.state.subtask}
+                            inputChange={(event) => this.handleInputChange(event)}/>
                         <FormButtons clickHandler={(event) => this.handleClickEvent(event)}/>
                     </div>
             </form> 
