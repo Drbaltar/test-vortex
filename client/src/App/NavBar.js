64a28562e69eb2abe919b3faecaa1b351e2fadd3
Drbaltar <chiefmccain85@gmail.com>
2019-10-25 15:45:14 -0600

    Fixed logo image

--- client/src/App/NavBar.js
+++ client/src/App/NavBar.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import VortexLogo from "./assets/tornado.png";
 
 const NavBar = (props) => {
     return(
         <nav className="navbar navbar-dark bg-dark fixed-top">
             <a className="navbar-brand" href="home">
-                <img src="./assets/tornado.png" width="30" height="30" class="d-inline-block align-top" alt=""/>
+                <img src={VortexLogo} width="35" height="30" className="d-inline-block align-top pr-1" alt=""/>
                 Test Vortex
             </a>
         </nav>
