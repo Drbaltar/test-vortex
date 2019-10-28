9101ae6ae6ced77af21cd234ec8462b5570ccdc6
Drbaltar <chiefmccain85@gmail.com>
2019-10-28 06:59:35 -0600

    Added 'Questions' link to navigation bar

--- client/src/App/NavBar.js
+++ client/src/App/NavBar.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import VortexLogo from "./assets/tornado.png";
+import VortexLogo from './assets/tornado.png';
+import { Link } from 'react-router-dom';
 
-const NavBar = (props) => {
+const NavBar = () => {
     return(
-        <nav className="navbar navbar-dark bg-dark fixed-top">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
             <a className="navbar-brand" href="home">
-                <img src={VortexLogo} width="35" height="30" className="d-inline-block align-top pr-1" alt=""/>
+                <img src={VortexLogo} width="30" height="30" className="d-inline-block align-top pr-1" alt=""/>
                 Test Vortex
             </a>
+            <div className="navbar-nav">
+                <Link to="/questions" className="nav-item nav-link">Questions</Link>     
+            </div>
         </nav>
     );
 };
