fec41917e163af0f36b21af01a884b76a686f944
Kyle <chiefmccain85@gmail.com>
2019-10-28 12:19:03 -0600

    Changed to Link component to keep from refreshing

--- client/src/App/NavBar.js
+++ client/src/App/NavBar.js
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
     return(
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-            <a className="navbar-brand" href="home">
+            <Link to="/home" className="navbar-brand">
                 <img src={VortexLogo} width="30" height="30" className="d-inline-block align-top pr-1" alt=""/>
                 Test Vortex
-            </a>
+            </Link>
             <div className="navbar-nav">
                 <Link to="/questions" className="nav-item nav-link">Questions</Link>     
             </div>
