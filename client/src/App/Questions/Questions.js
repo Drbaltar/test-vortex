823f8fee6e13ad4f9ff6ede448873b42623779d8
Kyle <chiefmccain85@gmail.com>
2019-11-06 16:12:58 -0700

    Updated formatting

--- client/src/App/Questions/Questions.js
+++ client/src/App/Questions/Questions.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import { Switch, Route, Link, useRouteMatch } from 'react-router-dom';
 
+import SideBar from './SideBar';
 import NewQuestion from './NewQuestion/InputForm';
+
+import "./Questions.css";
 
 const Questions = () => {
     const match = useRouteMatch();
 
     return(
-        <div>
-            <Link to={`${match.url}/add`} className="card">
-                Add card
-            </Link>
-        
-        <Switch>
-            <Route exact path={`${match.path}/add`}>
-                <NewQuestion/>
-            </Route>
-        </Switch>
-        </div>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="sidenav">
+                    <SideBar/>
+                </div>
+                
+                <Switch>
+                    <Route exact path={`${match.path}/add`}>
+                        <div className="col sub-body">
+                            <NewQuestion/>
+                        </div>
+                    </Route>
+                </Switch>
+            </div>
+        </div>  
     );
 }
 
