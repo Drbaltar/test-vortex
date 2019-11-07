9c293402704b4a00cfebdb37c0ce60dadf713bbc
Kyle <chiefmccain85@gmail.com>
2019-11-07 10:51:59 -0700

    Updated formatting for sidebar menu

--- client/src/App/Questions/SideBar.js
+++ client/src/App/Questions/SideBar.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import { NavLink, useRouteMatch } from 'react-router-dom';
 
+import "./SideBar.css";
+
 const SideBar = () => {
     const match = useRouteMatch();
     return(
-        <div className="pt-3">
-            <NavLink to={`${match.url}/add`} className="nav-item nav-link" 
+        <div className="pt-5">
+            <div className="pl-2 sidelabel" style={{font: 'bold'}}>Add Question</div>
+            <NavLink to={`${match.url}/add-Patriot`} className="nav-item nav-link pl-4" 
                 style={{color: 'black'}}
                 activeStyle={{
                     background: 'gray',
                     color:'white'
                 }}>
-                Add Question
+                Patriot
             </NavLink>
-            <NavLink to={`${match.url}/approve`} className="nav-item nav-link"
+            <hr/>
+            <div className="pl-2 sidelabel">Approve Question</div>
+            <NavLink to={`${match.url}/approve-Patriot`} className="nav-item nav-link pl-4"
                 style={{color: 'black'}}
                 activeStyle={{
                     background: 'gray',
                     color:'white'
                 }}>
-                Approve Question
+                Patriot
             </NavLink>
         </div>
     );
