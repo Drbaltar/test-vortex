dd7dd7e847c32089a63542175d13f6df942686a8
Drbaltar <chiefmccain85@gmail.com>
2019-11-09 10:29:19 -0700

    Updated SideBar to use props

--- client/src/App/Components/Questions/Questions.js
+++ client/src/App/Components/Questions/Questions.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, Link, useRouteMatch } from 'react-router-dom';
+import { Switch, Route, useRouteMatch } from 'react-router-dom';
 
 import SideBar from '../shared-components/SideBar/SideBar';
 import NewQuestion from './Components/NewQuestion/NewQuestion';
@@ -9,15 +9,23 @@ import "./Questions.css";
 const Questions = () => {
     const match = useRouteMatch();
 
+    const sideBarMenu = [{
+        title: 'Patriot Questions',
+        submenu: [{ title: 'Add (New)', link: 'add-patriot'},
+        { title: 'Approve (Pending)', link: 'approve-patriot'},
+        { title: 'Update (Existing)', link: 'update-patriot'},
+        { title: 'Delete (Existing)', link: 'delete-patriot'}]
+    }];
+
     return(
         <div className="container-fluid">
             <div className="row">
                 <div className="sidenav">
-                    <SideBar/>
+                    <SideBar menu={sideBarMenu}/>
                 </div>
                 
                 <Switch>
-                    <Route exact path={`${match.path}/add-Patriot`}>
+                    <Route exact path={`${match.path}/add-patriot`}>
                         <div className="col sub-body">
                             <NewQuestion/>
                         </div>
--- client/src/App/Components/shared-components/SideBar/SideBar.js
+++ client/src/App/Components/shared-components/SideBar/SideBar.js
@@ -3,44 +3,30 @@ import { NavLink, useRouteMatch } from 'react-router-dom';
 
 import "./SideBar.css";
 
-const SideBar = () => {
+const SideBar = (props) => {
+    let menuList = props.menu;
+
     const match = useRouteMatch();
-    return(
-        <div className="pt-5">
-            <div className="pl-2 sidelabel" style={{font: 'bold'}}>Patriot Questions</div>
-            <NavLink to={`${match.url}/add-patriot`} className="nav-item nav-link pl-4" 
-                style={{color: 'black'}}
-                activeStyle={{
-                    background: 'gray',
-                    color:'white'
-                }}>
-                Add (New)
-            </NavLink>
-            <NavLink to={`${match.url}/approve-patriot`} className="nav-item nav-link pl-4"
-                style={{color: 'black'}}
-                activeStyle={{
-                    background: 'gray',
-                    color:'white'
-                }}>
-                Approve (Pending)
-            </NavLink>
-            <NavLink to={`${match.url}/update-patriot`} className="nav-item nav-link pl-4"
-                style={{color: 'black'}}
-                activeStyle={{
-                    background: 'gray',
-                    color:'white'
-                }}>
-                Update (Existing)
-            </NavLink>
-            <NavLink to={`${match.url}/delete-patriot`} className="nav-item nav-link pl-4"
-                style={{color: 'black'}}
-                activeStyle={{
-                    background: 'gray',
-                    color:'white'
-                }}>
-                Delete (Existing)
-            </NavLink>
+    const filledMenu = menuList.map(entry => 
+        <div key={entry.title}>
+            <div className="pl-2 sidelabel" style={{font: 'bold'}}>{entry.title}</div>
+            {entry.submenu.map(entry => 
+                <NavLink key={entry.title} to={`${match.url}/${entry.link}`} className="nav-item nav-link pl-4" 
+                    style={{color: 'black'}}
+                    activeStyle={{
+                        background: 'gray',
+                        color:'white'
+                    }}>
+                    {entry.title}
+                </NavLink>
+                )}
         </div>
+        );
+        
+        return(
+            <div className="pt-5">
+                {filledMenu}
+            </div>
     );
 }
 
