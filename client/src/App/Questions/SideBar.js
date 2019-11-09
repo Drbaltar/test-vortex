39c2e59bff436ea28821fbc69537169fcd736996
Drbaltar <chiefmccain85@gmail.com>
2019-11-09 08:41:32 -0700

    Created links to CRUD patriot questions

--- client/src/App/Questions/SideBar.js
+++ client/src/App/Questions/SideBar.js
@@ -8,7 +8,7 @@ const SideBar = () => {
     return(
         <div className="pt-5">
             <div className="pl-2 sidelabel" style={{font: 'bold'}}>Add Question</div>
-            <NavLink to={`${match.url}/add-Patriot`} className="nav-item nav-link pl-4" 
+            <NavLink to={`${match.url}/add-patriot`} className="nav-item nav-link pl-4" 
                 style={{color: 'black'}}
                 activeStyle={{
                     background: 'gray',
@@ -18,7 +18,27 @@ const SideBar = () => {
             </NavLink>
             <hr/>
             <div className="pl-2 sidelabel">Approve Question</div>
-            <NavLink to={`${match.url}/approve-Patriot`} className="nav-item nav-link pl-4"
+            <NavLink to={`${match.url}/approve-patriot`} className="nav-item nav-link pl-4"
+                style={{color: 'black'}}
+                activeStyle={{
+                    background: 'gray',
+                    color:'white'
+                }}>
+                Patriot
+            </NavLink>
+            <hr/>
+            <div className="pl-2 sidelabel">Update Question</div>
+            <NavLink to={`${match.url}/update-patriot`} className="nav-item nav-link pl-4"
+                style={{color: 'black'}}
+                activeStyle={{
+                    background: 'gray',
+                    color:'white'
+                }}>
+                Patriot
+            </NavLink>
+            <hr/>
+            <div className="pl-2 sidelabel">Delete Question</div>
+            <NavLink to={`${match.url}/delete-patriot`} className="nav-item nav-link pl-4"
                 style={{color: 'black'}}
                 activeStyle={{
                     background: 'gray',
