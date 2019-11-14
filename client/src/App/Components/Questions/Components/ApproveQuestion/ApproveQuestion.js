323e02e19f93a3ce66b8d3e18de32fa5d96cf89c
Kyle <chiefmccain85@gmail.com>
2019-11-14 09:19:27 -0700

    Initial commit

---
+++ client/src/App/Components/Questions/Components/ApproveQuestion/ApproveQuestion.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Axios from 'axios';
+
+class ApproveQuestion extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.initialState = {
+            
+        }
+    }
+
+}
+
+export default ApproveQuestion;
