a1b8bf8f0d81a70fd121a3ad8ab9f246bedb7946
Kyle <chiefmccain85@gmail.com>
2019-11-14 11:27:20 -0700

    Initial commit

---
+++ server/src/database-interface.js
@@ -0,0 +1,18 @@
+// const Issue = require('../models/issue');
+const MultQuestion = require('../models/patriot/mult-question');
+// const TFQuestion = require('../models/patriot/tf-question');
+// const FillBlankQuestion = require('../models/patriot/fill-blank-question');
+
+
+// Returns the entire collection of pending questions
+const getAllPendingQuestions = () => {
+    // Use the MultQuestion model to query for the pending questions, but the 
+    // result will include all 'True or False' and 'Fill-in-the-Blank' as well
+    MultQuestion.PendingMultQuestion.find().exec((err, response) => {
+        console.log(response);
+    });
+};
+
+module.exports = {
+    getAllPendingQuestions
+};
