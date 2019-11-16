b5b4f8386b40f1289e37aeca259d30c7f1e096b5
Drbaltar <chiefmccain85@gmail.com>
2019-11-16 09:08:53 -0700

    Added detailed view and loading spinner

--- client/src/App/Components/Questions/Components/ApproveQuestion/ApproveQuestion.js
+++ client/src/App/Components/Questions/Components/ApproveQuestion/ApproveQuestion.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Axios from 'axios';
 
-import FormButtons from '../../../shared-components/FormButtons';
 import PendingQuestionList from './Components/PendingQuestionList';
+import QuestionForm from '../QuestionForm/QuestionForm';
 
 class ApproveQuestion extends React.Component {
     constructor(props) {
@@ -11,6 +11,7 @@ class ApproveQuestion extends React.Component {
         this.initialState = {
             allPendingQuestions: [],
             loading: false,
+            selectedEntry: null,
             detailedView: false
         };
 
@@ -19,33 +20,68 @@ class ApproveQuestion extends React.Component {
 
     handleClickEvent = (event) => {
         event.preventDefault();
-        if (event.target.id === 'clearAllButton') {
-            this.setState(this.initialState);
-        } else if (event.target.id === 'submitButton') {
+        if (event.target.id === 'queryButton') {
             this.getAllPendingQuestions();
-        }
+        };
+    };
+
+    showDetails = (index) => {
+        this.setState({ detailedView: true, selectedEntry: index})
+    };
+
+    returnToMenu = () => {
+        this.setState({ detailedView: false, selectedEntry: null})
     };
 
     getAllPendingQuestions = () => {
+        this.setState({loading: true});
+
         Axios.get('/api/questions/pending')
-            .then((response) => this.setState({allPendingQuestions: response.data}))
-    }
+            .then((response) => this.setState({allPendingQuestions: response.data, loading: false}))
+    };
 
     render() {
         let approveQuestionView;
 
         if (!this.state.detailedView) {
+            let loadingSpinner;
+
+            if (this.state.loading) {
+                loadingSpinner = (
+                    <div class="d-flex justify-content-center mb-3">
+                        <div class="spinner-border" role="status">
+                            <span class="sr-only">Loading...</span>
+                        </div>
+                    </div>
+                )
+            }
+            
             approveQuestionView = (
                 <form className="card bg-light" noValidate>
                     <h1 className="card-header">Approve Pending Questions ({this.props.questionType})</h1>
-                    <div className="p-4">
-                        <PendingQuestionList list={this.state.allPendingQuestions}/>
+                    <div className="pt-3 pr-3">
+                        <div className="btn btn-primary" style={{float: "right"}} id="queryButton"
+                            onClick={(event) => this.handleClickEvent(event)}>
+                            &#8635; Refresh List
+                        </div>
                     </div>
-                    <FormButtons clickHandler={(event) => this.handleClickEvent(event)}/>
+                    <div className="p-4">
+                        {loadingSpinner}
+                        <PendingQuestionList list={this.state.allPendingQuestions}
+                            showDetails={(index) => this.showDetails(index)}/>
+                    </div>
                 </form>
             )
         } else {
-
+            approveQuestionView = (
+                <div>
+                    <div className="btn btn-secondary mt-4" style={{marginLeft: "20px"}} id="returnButton"
+                        onClick={this.returnToMenu}>
+                        &#8678; Go Back
+                    </div>
+                    <QuestionForm title="Question Details" data={this.state.allPendingQuestions[this.state.selectedEntry]}/>
+                </div>
+            )
         }
 
         return (
