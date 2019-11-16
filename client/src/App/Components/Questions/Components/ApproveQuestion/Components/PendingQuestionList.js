0fab9ed11c85504815ca3c46ce249251838f7cb6
Drbaltar <chiefmccain85@gmail.com>
2019-11-16 09:10:00 -0700

    Added 'Test Type' and used index from map function

--- client/src/App/Components/Questions/Components/ApproveQuestion/Components/PendingQuestionList.js
+++ client/src/App/Components/Questions/Components/ApproveQuestion/Components/PendingQuestionList.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 const PendingQuestionList = (props) => {
     let pendingQuestionList = props.list;
-    let index = 0;
 
-    const filledList = pendingQuestionList.map(entry =>
-        <div type="button" className="btn btn-block btn-outline-secondary">
-            <div className="row" key={index++}>
+    const filledList = pendingQuestionList.map((entry, index) =>
+        <div key={index} className="btn btn-block btn-outline-dark p-3"
+        onClick={() => props.showDetails(index)}>
+            <div className="row">
+                <div className="col-sm">
+                    <label>Test Type:</label>
+                    <input readOnly={true} className="form-control" type="text" placeholder={entry.gunnery_table[0].test_type}></input>
+                </div>
                 <div className="col-sm">
                     <label>Question Type:</label>
                     <input readOnly={true} className="form-control" type="text" placeholder={entry.question_type}></input>
