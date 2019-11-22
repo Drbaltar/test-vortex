c8b1ba50d049343068e60eb594114a9892f2ebf6
Kyle <chiefmccain85@gmail.com>
2019-11-22 12:24:17 -0700

    Added route to be able to approve pending question

--- server/routes/question.js
+++ server/routes/question.js
@@ -44,6 +44,47 @@ router.post('/submit', (req, res) => {
     });
 });
 
+// Route for approving a question and moving to the official question bank
+router.post('/approve', (req, res) => {
+    // Declare new question
+    let approvedQuestion;
+
+    // Find out what type of question is being submitted and build appropriate document
+    switch (req.body.questionType) {
+    case 'Multiple Choice':
+        approvedQuestion = documentBuilder.buildMultQuestionDocument('approved', req);
+        break;
+    case 'True or False':
+        approvedQuestion = documentBuilder.buildTFQuestionDocument('approved', req);
+        break;
+    case 'Fill-in-the-Blank':
+        approvedQuestion = documentBuilder.buildFillBlankQuestionDocument('approved', req);
+        break;
+    default:
+        res.status(400).send({message: 'The \'Question Type\' entry is not a valid entry'});
+        return;
+    }
+
+    // Validate document requirements before going any further
+    approvedQuestion.validate((err) => {
+        if (err) {
+            console.log('Validation Failed: ' + err);
+            res.status(400).send(err);
+        } else {
+            // Save new Question document to the database
+            approvedQuestion.save()
+                .then((result) => {
+                    console.log(result);
+                    res.send('The question has been added to the question bank! The question can be referenced by the following value: ' + result._id);
+                })
+                .catch((err) => {
+                    console.log('Error: ' + err);
+                    res.status(500).send(err);
+                });
+        }
+    });
+});
+
 // Route for getting questions that were submitted and are pending approval
 router.get('/pending', (req, res) => {
     dbInterface.getAllPendingQuestions((err, queryResults) => {
