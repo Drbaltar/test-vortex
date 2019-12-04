927e37f75271993ca1d6b77452241abc647d13b7
Drbaltar <chiefmccain85@gmail.com>
2019-12-03 20:38:46 -0700

    Added functions to get and delete pending question

--- server/src/database-interface.js
+++ server/src/database-interface.js
@@ -13,33 +13,43 @@ const getAllPendingQuestions = (callback) => {
     });
 };
 
-// Returns the appropriate document for the entry id passed in
-const getPendingQuestion = (data, callback) => {
+// Returns the appropriate document for the entry ID and question type passed in
+const getPendingQuestion = (id, questionType, callback) => {
     // Declare the pending question entry
     let entry;
 
     // Find out what type of question is being submitted and build appropriate document
-    switch (data.questionType) {
+    switch (questionType) {
     case 'Multiple Choice':
-        entry = new MultQuestion.PendingMultQuestion;
+        entry = MultQuestion.PendingMultQuestion;
         break;
     case 'True or False':
-        entry = new TFQuestion.PendingTFQuestion;
+        entry = TFQuestion.PendingTFQuestion;
         break;
     case 'Fill-in-the-Blank':
-        entry = new FillBlankQuestion.PendingFillBlankQuestion;
+        entry = FillBlankQuestion.PendingFillBlankQuestion;
         break;
     default:
         callback (null, 'The \'Question Type\' entry is not a valid entry');
         return;
     }
 
-    entry.findById(data._id, (err, doc) => {
+    entry.findById(id, (err, doc) => {
         callback(err, doc);
+    });
+};
+
+// Deletes the pending question entry based on the passed in ID
+const deletePendingQuestion = (id, callback) => {
+    // Use the MultQuestion model to query for the pending questions, but the 
+    // result will include all 'True or False' and 'Fill-in-the-Blank' as well
+    MultQuestion.PendingMultQuestion.findByIdAndDelete(id, (err, response) => {
+        callback(err, response);
     });
 };
 
 module.exports = {
     getAllPendingQuestions,
-    getPendingQuestion
+    getPendingQuestion,
+    deletePendingQuestion
 };
