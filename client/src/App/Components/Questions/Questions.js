a8065d89b33789aef9a093ba138fa582ade95eba
Kyle <chiefmccain85@gmail.com>
2019-12-18 14:03:54 -0700

    Added route to delete an existing Patriot question

--- client/src/App/Components/Questions/Questions.js
+++ client/src/App/Components/Questions/Questions.js
@@ -4,7 +4,8 @@ import { Switch, Route, useRouteMatch } from 'react-router-dom';
 import SideBar from '../shared-components/SideBar/SideBar';
 import NewQuestion from './Components/NewQuestion/NewQuestion';
 import ApproveQuestion from './Components/ApproveQuestion/ApproveQuestion';
-import UpdateQuestion from './Components/UpdateQuestion/UpdateQuestion'
+import UpdateQuestion from './Components/UpdateQuestion/UpdateQuestion';
+import DeleteQuestion from './Components/DeleteQuestion/DeleteQuestion';
 
 const Questions = () => {
     const match = useRouteMatch();
@@ -38,6 +39,11 @@ const Questions = () => {
                     <Route exact path={`${match.path}/update-patriot`}>
                         <div className="col sub-body">
                             <UpdateQuestion questionType="Patriot"/>
+                        </div>
+                    </Route>
+                    <Route exact path={`${match.path}/delete-patriot`}>
+                        <div className="col sub-body">
+                            <DeleteQuestion questionType="Patriot"/>
                         </div>
                     </Route>
                 </Switch>
