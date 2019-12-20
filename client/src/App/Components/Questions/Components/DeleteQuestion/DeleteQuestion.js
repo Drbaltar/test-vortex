d5c3d5b85a54dba9c3805fd15468d9901758fcd7
Kyle <chiefmccain85@gmail.com>
2019-12-20 08:06:37 -0700

    Implemented SingleQuestionSearch component

--- client/src/App/Components/Questions/Components/DeleteQuestion/DeleteQuestion.js
+++ client/src/App/Components/Questions/Components/DeleteQuestion/DeleteQuestion.js
@@ -1,53 +1,23 @@
 import React from 'react';
 import Axios from 'axios';
 
-import QuestionList from '../QuestionList/QuestionList';
-import QuestionForm from '../QuestionForm/QuestionForm';
+import SingleQuestionSearch from '../SingleQuestionSearch/SingleQuestionSearch';
 
 class DeleteQuestion extends React.Component {
     constructor(props) {
         super(props);
 
         this.initialState = {
-            searchInput: '',
-            searchResults: null,
-            searchReturned: false,
-            loading: false,
-            detailedView: false,
-            submissionResponse: null,
+            submissionResponse: '',
             successAlert: false
-        };
+        }
 
         this.state = this.initialState;
-    };
+    }
 
-    handleInputChange = (event) => {
-        const {target: { id, value}} = event;
-        this.setState({[id]: value});
-    };
-
-    handleClickEvent = (event) => {
-        event.preventDefault();
-        if (event.target.id === 'queryButton') {
-            this.getSearchResults();
-        };
-    };
-
-    showDetails = () => {
-        this.setState({ detailedView: true})
-    };
-
-    returnToMenu = () => {
-        this.setState({ detailedView: false})
-    };
-
-    getSearchResults = () => {
-        this.setState({searchResults: null, searchReturned: false, loading: true});
-
-        Axios.get('/api/questions/search', { params: { _id: this.state.searchInput } })
-            .then((response) => this.setState({searchResults: response.data, searchReturned: true, loading: false}))
-            .catch((response) => this.setState({submissionResponse: response, loading: false}))
-    };
+    updateSubmissionResponse = (response) => {
+        this.setState({ submissionResponse: response })
+    }
 
     deleteQuestion = (questionData) => {        
         Axios.delete('/api/questions/delete', { params: { _id: questionData._id } })
@@ -55,96 +25,32 @@ class DeleteQuestion extends React.Component {
             .catch((response) => this.setState({submissionResponse: response}))
     };
 
-    displaySearchResults = () => {
-        let loadingSpinner;
-        let searchResults;
+    render(){
+        let deleteQuestionView;
 
-        if (this.state.loading) {
-            loadingSpinner = (
-                <div className="d-flex justify-content-center mb-3">
-                    <div className="spinner-border" role="status">
-                        <span className="sr-only">Loading...</span>
-                    </div>
+        if (this.state.successAlert) {
+            deleteQuestionView = (
+                <div className="alert alert-success" role="alert">
+                    {this.state.submissionResponse.data}
+                </div>
+            )
+        } else {
+            deleteQuestionView = (
+                <div>
+                    <SingleQuestionSearch questionType={this.props.questionType}
+                        submitButtonText="Delete" submitEvent={(questionData) => this.deleteQuestion(questionData)}
+                        cancelButtonText="Revert Changes"
+                        submissionResponse={this.state.submissionResponse} successAlert={this.state.successAlert}
+                        updateSubmissionResponse={(response) => this.updateSubmissionResponse(response)}/>
                 </div>
             )
         }
-
-        if (this.state.searchResults) {
-            searchResults = (
-                <QuestionList list={[this.state.searchResults]}
-                    showDetails={() => this.showDetails()}/>
-            )
-        } else {
-            if (this.state.searchReturned) {
-                searchResults = (
-                    <div>
-                        <p>
-                            No Search Results Found!
-                        </p>
-                    </div>
-                )
-            }
-        }
-
-        return (
-            <div className="p-4">
-                {loadingSpinner}
-                {searchResults}
-            </div>
-        )
-    };
-
-    render() {
-        let deleteQuestionView;
-
-        if (!this.state.detailedView) {
-            deleteQuestionView = (
-                <form className="card bg-light" noValidate>
-                    <h1 className="card-header">Delete Existing Question ({this.props.questionType})</h1>
-                    <label className="pt-3 pl-3" htmlFor={"searchInput"}>Question ID</label>
-                    <div className="form-group ml-3 mr-3">
-                        <div className="row">
-                            <input className="form-control ml-3 col-6" type="text" id="searchInput" value={this.state.searchInput}
-                                    placeholder={'Please enter the Question ID'}
-                                    onChange={(e) => {this.handleInputChange(e)}}/>
-                                <div className="btn btn-primary ml-3"  id="queryButton"
-                                    onClick={(e) => this.handleClickEvent(e)}>
-                                    Search
-                                </div>
-                        </div>
-                    </div>
-                    {this.displaySearchResults()}
-
-                </form>
-            )
-        } else {
-            if (this.state.successAlert) {
-                deleteQuestionView = (
-                    <div className="alert alert-success" role="alert">
-                        {this.state.submissionResponse.data}
-                    </div>
-            )} else {
-                deleteQuestionView = (
-                    <div>
-                        <div className="btn btn-secondary mt-4" style={{marginLeft: "20px"}} id="returnButton"
-                            onClick={this.returnToMenu}>
-                            &#8678; Go Back
-                        </div>
-                        <QuestionForm title="Question Details" data={this.state.searchResults}
-                            submitButtonText="Delete" cancelButtonText="Revert Changes"
-                            submitEvent={(questionData) => this.deleteQuestion(questionData)}/>
-                    </div>
-                )
-            }
-        }
-
         return (
             <div>
                 {deleteQuestionView}
             </div>
         )
     };
-
 }
 
 export default DeleteQuestion;
