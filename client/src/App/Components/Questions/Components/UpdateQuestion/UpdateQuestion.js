7275f890feb591e13de573595cd6c35b10801b5a
Kyle <chiefmccain85@gmail.com>
2020-03-10 13:26:47 -0600

    Added the SuccessMessage component

--- client/src/App/Components/Questions/Components/UpdateQuestion/UpdateQuestion.js
+++ client/src/App/Components/Questions/Components/UpdateQuestion/UpdateQuestion.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Axios from 'axios';
 
 import SingleQuestionSearch from '../SingleQuestionSearch/SingleQuestionSearch';
+import SuccessMessage from '../../../shared-components/SuccessMessage/SuccessMessage';
 
 class updateQuestion extends React.Component {
     constructor(props) {
@@ -19,6 +20,10 @@ class updateQuestion extends React.Component {
         this.setState({ submissionResponse: response })
     }
 
+    returnToMenu = () => {
+        this.setState({submissionResponse: '', successAlert: false})
+    };
+
     updateQuestion = (questionData) => {
         Axios.put('/api/questions/update', questionData)
             .then((response) => this.setState({submissionResponse: response, successAlert: true}))
@@ -30,9 +35,8 @@ class updateQuestion extends React.Component {
 
         if (this.state.successAlert) {
             updateQuestionView = (
-                <div className="alert alert-success" role="alert">
-                    {this.state.submissionResponse.data}
-                </div>
+                <SuccessMessage message={this.state.submissionResponse.data}
+                    clickHandler={() => this.returnToMenu()}/>
             )
         } else {
             updateQuestionView = (
