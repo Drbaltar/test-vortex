fd8fed4c3b3d4f56a5c4ac43c31624c35630dcb3
Kyle <chiefmccain85@gmail.com>
2020-03-10 19:28:27 -0600

    Added Delete Button and made all buttons optional

--- client/src/App/Components/shared-components/FormButtons.js
+++ client/src/App/Components/shared-components/FormButtons.js
@@ -1,12 +1,36 @@
 import React from 'react';
 
 const FormButtons = (props) => {
-    return(
-        <div className="form-buttons">
+    let submitButton;
+    let cancelButton;
+    let deleteButton;
+
+    if (props.submitButtonText) {
+        submitButton = (
             <button className="btn btn-success" id={props.submitButtonID}
                 onClick={props.clickHandler}>{props.submitButtonText}</button>
+        )
+    };
+
+    if (props.cancelButtonText) {
+        cancelButton = (
             <button className="btn btn-secondary ml-4" id={props.cancelButtonID}
                 onClick={props.clickHandler}>{props.cancelButtonText}</button>
+        )
+    };
+
+    if (props.deleteButtonText) {
+        deleteButton = (
+            <button className="btn btn-danger ml-4" id={props.deleteButtonID}
+                onClick={props.clickHandler}>{props.deleteButtonText}</button>
+        )
+    };
+
+    return(
+        <div className="form-buttons">
+            {submitButton}
+            {cancelButton}
+            {deleteButton}
         </div>
     );
 };
