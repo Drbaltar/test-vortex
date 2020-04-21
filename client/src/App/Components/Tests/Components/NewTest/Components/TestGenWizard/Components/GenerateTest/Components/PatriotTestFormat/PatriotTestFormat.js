27594b31e7086a7f013c34b3734baffd0f525904
Kyle <chiefmccain85@gmail.com>
2020-04-21 08:51:30 -0600

    Added questions and answers to the format

--- client/src/App/Components/Tests/Components/NewTest/Components/TestGenWizard/Components/GenerateTest/Components/PatriotTestFormat/PatriotTestFormat.js
+++ client/src/App/Components/Tests/Components/NewTest/Components/TestGenWizard/Components/GenerateTest/Components/PatriotTestFormat/PatriotTestFormat.js
@@ -1,4 +1,33 @@
-const PatriotTestFormat = (unitType, testLevel, testType, version, date) => {
+const PatriotTestFormat = (unitType, testLevel, testType, version, date, testQuestions) => {
+    let questionNumber = 1;
+
+    let testQuestionsString = testQuestions.map((question) => {
+        if (question.question_type === 'Multiple Choice') {
+            return([{
+                text: `${questionNumber++})\t${question.question_description}`,
+                style: 'questions'
+            },
+            {
+                text: `a) ${question.correct_answer}\n` +
+                    `b) ${question.answer_a}\n` +
+                    `c) ${question.answer_b}\n` +
+                    `d) ${question.answer_c}\n\n`,
+                style: 'answers'
+            }])
+        } else if (question.question_type === 'True or False') {
+            return({
+                text: `${questionNumber++})\tTrue or False: ${question.question_description}\n\n`,
+                style: 'questions'
+            })
+        } else {
+            return({
+                text: `${questionNumber++})\t${question.question_description}\n\n`,
+                style: 'questions'
+            })
+        }
+
+    })
+
     return ({
         content: [
             {
@@ -11,7 +40,8 @@ const PatriotTestFormat = (unitType, testLevel, testType, version, date) => {
             {
                 text: `Version ${version || 'A'}\nCreated on ${date || '17APR20'}`,
                 style: 'titlePageInfo'
-            }
+            },
+            testQuestionsString
         ],
 
         styles: {
@@ -24,9 +54,17 @@ const PatriotTestFormat = (unitType, testLevel, testType, version, date) => {
                 fontSize: 14,
                 bold: true,
                 alignment: 'center'
+            },
+            questions: {
+                fontSize: 14
+            },
+            answers: {
+                fontSize: 14,
+                margin: [26, 8, 0, 0]
             }
-        }
+        },
 
+        pageMargins: 60
     })
 };
 
