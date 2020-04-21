7fc353d81650d7e093d901df708cc311ac588bda
Kyle <chiefmccain85@gmail.com>
2020-04-21 08:52:51 -0600

    Added spinner to show questions are being loaded

--- client/src/App/Components/Tests/Components/NewTest/Components/TestGenWizard/Components/QuestionReview/QuestionReview.js
+++ client/src/App/Components/Tests/Components/NewTest/Components/TestGenWizard/Components/QuestionReview/QuestionReview.js
@@ -13,9 +13,25 @@ class QuestionReview extends React.Component {
     }
 
     render() {
+        let loadingSpinner;
+
+        if (this.props.loadingQuestions) {
+            loadingSpinner = (
+                <div className="d-flex justify-content-center mb-3">
+                    <div className="spinner-border" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                </div>
+            );
+        }
+
         return(
             <div className='p-4'>
                 <h2 style={{'textAlign': 'center'}}>Review All Test Questions</h2>
+                <div>
+                    {/* <p>{this.props.testQuestions}</p> */}
+                </div>
+                {loadingSpinner}
                 <NavigationButtons  previousButton={true} nextButton={true} 
                     isNextButtonDisabled={!this.isAllFieldsComplete()}
                     clickHandler={this.props.clickHandler}/>
