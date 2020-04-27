35ecd735ed8713010027b2d50d8563375abcce08
Kyle <chiefmccain85@gmail.com>
2020-04-27 14:03:19 -0600

    Converted to function from class and moved state

--- client/src/App/Components/Questions/Components/QuestionForm/Components/TopicCategories/TopicCategories.js
+++ client/src/App/Components/Questions/Components/QuestionForm/Components/TopicCategories/TopicCategories.js
@@ -1,59 +1,33 @@
 import React from 'react';
-import Axios from 'axios';
 
 import TopicCategoryDropdown from './Components/TopicsCategoryDropdown/TopicCategoryDropdown';
 
-class TopicCategories extends React.Component {
-    constructor(props) {
-        super(props);
+const TopicCategories = (props) => {
+    let majorTopics = [];
+    let subTopics = [];
 
-        this.state = {
-            existingTopicCategories: null
+    if (props.existingTopicCategories !== null && Object.keys(props.existingTopicCategories).length !== 0) {
+        majorTopics = Object.keys(props.existingTopicCategories).sort();
+        if (majorTopics.includes(props.topic.majorCategory)) {
+            subTopics = props.existingTopicCategories[props.topic.majorCategory].sort();
         }
     }
 
-    getExistingTopics = (event) => {
-        event.preventDefault();
-        if (this.props.gunneryTable.length === 0) {
-            return;
-        }
-
-        Axios.get('/api/questions/topics', { params: { table: this.props.gunneryTable[0].table, 
-            subtask: this.props.gunneryTable[0].subtask}})
-            .then((response) => this.setState({existingTopicCategories: response.data}))
-            .catch((response) => {
-                this.setState({existingTopicCategories: null});
-            });
-    }
-
-    render() {
-        let majorTopics = [];
-        let subTopics = [];
-
-        if (this.state.existingTopicCategories !== null && Object.keys(this.state.existingTopicCategories).length !== 0) {
-            majorTopics = Object.keys(this.state.existingTopicCategories).sort();
-            if (majorTopics.includes(this.props.topic.majorCategory)) {
-                subTopics = this.state.existingTopicCategories[this.props.topic.majorCategory].sort();
-            }
-        }
-
-        return (
-            <div className="border border-muted rounded mt-3 p-3">
-                <small id="multChoiceAnswerLabel" className="form-text">Select or input a Major Category and Sub-Category for your question topic:</small>
-                <button className='btn btn-primary' onClick={(event) => this.getExistingTopics(event)}>Update Topics</button>
-                <TopicCategoryDropdown categoryType='Major Category' inputCategory={this.props.topic.majorCategory} 
-                    categoryID='majorCategory' inputChange={(event) => this.props.inputChange(event)}
-                    isValid={this.props.isMajorTopicValid} existingTopicCategories={majorTopics}
-                    errorMessage={'You must select or input a Major Category for your question topic!'}
-                    topicChange={(category, value) => this.props.topicChange(category, value)}/>
-                <TopicCategoryDropdown categoryType='Sub-Category' inputCategory={this.props.topic.subCategory}
-                    categoryID='subCategory' inputChange={(event) => this.props.inputChange(event)}
-                    isValid={this.props.isSubTopicValid} existingTopicCategories={subTopics}
-                    errorMessage={'You must select or input a Sub-Category for your question topic!'}
-                    topicChange={(category, value) => this.props.topicChange(category, value)}/>
-            </div>
-        )
-    }
+    return (
+        <div className="border border-muted rounded mt-3 p-3">
+            <small id="multChoiceAnswerLabel" className="form-text">Select or input a Major Category and Sub-Category for your question topic:</small>
+            <TopicCategoryDropdown categoryType='Major Category' inputCategory={props.topic.majorCategory} 
+                categoryID='majorCategory' inputChange={(event) => props.inputChange(event)}
+                isValid={props.isMajorTopicValid} existingTopicCategories={majorTopics}
+                errorMessage={'You must select or input a Major Category for your question topic!'}
+                topicChange={(category, value) => props.topicChange(category, value)}/>
+            <TopicCategoryDropdown categoryType='Sub-Category' inputCategory={props.topic.subCategory}
+                categoryID='subCategory' inputChange={(event) => props.inputChange(event)}
+                isValid={props.isSubTopicValid} existingTopicCategories={subTopics}
+                errorMessage={'You must select or input a Sub-Category for your question topic!'}
+                topicChange={(category, value) => props.topicChange(category, value)}/>
+        </div>
+    )
 }
 
 export default TopicCategories;
