ad401d3651ee21d3c081c4e88ef2829b83b41dee
Kyle <chiefmccain85@gmail.com>
2020-05-18 16:02:42 -0600

    Added new layout for different test versions

--- client/src/App/Components/Tests/Components/NewTest/Components/TestGenWizard/Components/GenerateTest/GenerateTest.js
+++ client/src/App/Components/Tests/Components/NewTest/Components/TestGenWizard/Components/GenerateTest/GenerateTest.js
@@ -1,47 +1,111 @@
 import React from 'react';
+
+import TestVersionCard from './Components/TestVersionCard/TestVersionCard';
+import NavigationButtons from '../shared-components/NavigationButtons';
+
 import pdfMake from 'pdfmake/build/pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
-import NavigationButtons from '../shared-components/NavigationButtons';
 import PatriotTestFormat from './Components/PatriotTestFormat/PatriotTestFormat';
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
-
 class GenerateTest extends React.Component {
+
     constructor(props) {
         super(props);
+
+        this.state = {
+            date: this.getTodaysDateString()
+        }
     }
     
-    // Returns true if all fields have inputs
-    isAllFieldsComplete = () => {
-        return true;
-    }
-
     // Creates a string of the current date in the DDMMMYY format
     getTodaysDateString = () => {
         const today = new Date(Date.now());
         return `${today.getDate()}${new Intl.DateTimeFormat('en-US', {month: 'short'}).format(today).toUpperCase()}${today.getYear()-100}`;
     }
 
+    // Create document definition for the test
+    createTestDefinition = (action) => {
+        let version;
+        let questions;
+
+        if (action === 'openTestA' || action === 'downloadTestA') {
+            version = 'A';
+            questions = this.props.testVersions[0];
+        } else if (action === 'openTestB' || action === 'downloadTestB') {
+            version = 'B';
+            questions = this.props.testVersions[1];
+        }
+
+        return PatriotTestFormat(this.props.unitType, this.props.testLevel, 
+            this.props.testType, version, this.state.date, questions);
+    }
+
+    // Open the newly generated test
+    openTest = (event) => {
+        event.preventDefault();
+        
+        pdfMake.createPdf(this.createTestDefinition(event.target.id)).open();
+    }
+
     // Download the newly generated test
     downloadTest = (event) => {
         event.preventDefault();
+        
+        pdfMake.createPdf(this.createTestDefinition(event.target.id)).download();
+    }
+
+    // Open the newly generated answer key
+    openKey = (event) => {
+        event.preventDefault();
+
+        let version;
+        let questions;
+        if (event.target.id === 'openKeyA') {
+            version = 'A';
+            questions = this.props.versionA;
+        } else if (event.target.id === 'openKeyB') {
+            version = 'B';
+            questions = this.props.versionB;
+        }
 
         let docDefinition = PatriotTestFormat(this.props.unitType, this.props.testLevel, 
-            this.props.testType, 'A', this.getTodaysDateString(), this.props.testQuestions);
+            this.props.testType, version, this.state.date, this.props.testQuestions);
         
         pdfMake.createPdf(docDefinition).open();
+    }
+
+    // Download the newly generated answer key
+    downloadKey = (event) => {
+        event.preventDefault();
+
+        let version;
+        let questions;
+        if (event.target.id === 'openKeyA') {
+            version = 'A';
+            questions = this.props.versionA;
+        } else if (event.target.id === 'openKeyB') {
+            version = 'B';
+            questions = this.props.versionB;
+        }
+
+        let docDefinition = PatriotTestFormat(this.props.unitType, this.props.testLevel, 
+            this.props.testType, version, this.state.date, this.props.testQuestions);
         
+        pdfMake.createPdf(docDefinition).open();
     }
 
     render() {
-
         return(
             <div className='p-4'>
                 <h2 style={{'textAlign': 'center'}}>Choose to Generate a Test or Save for Later</h2>
-                <button className="btn btn-secondary"onClick={(event) => this.downloadTest(event)}>Open Test!</button>
+                <TestVersionCard version='A' openTest={(event) => this.openTest(event)}
+                    downloadTest={(event) => this.downloadTest(event)}/>
+                <TestVersionCard version='B' openTest={(event) => this.openTest(event)}
+                    downloadTest={(event) => this.downloadTest(event)}/>
                 <NavigationButtons  previousButton={true} nextButton={true} 
-                    nextButtonID='submitButton' nextButtonText='Submit'
-                    isNextButtonDisabled={!this.isAllFieldsComplete()}
+                    nextButtonID='submitButton' nextButtonText='Save Test'
+                    isNextButtonDisabled={false}
                     clickHandler={this.props.clickHandler}/>
             </div> 
         );
