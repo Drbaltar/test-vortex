6b4ec618122e2fa529702c046811688c434ae0f7
Kyle <chiefmccain85@gmail.com>
2020-07-17 16:47:57 -0600

    Added routes to add new tests and receive previous

--- server/routes/test.js
+++ server/routes/test.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const patriotTestGen = require('../src/test generation/patriot-test-gen');
+const Test = require('../models/patriot/test');
 
 // Route for getting questions for a new patriot test with automatic selection of questions
 router.get('/new-patriot-auto', (req, res) => {
@@ -23,7 +24,7 @@ router.get('/new-patriot-auto', (req, res) => {
             });
     } else {
         if (!unitType) {
-            res.status(400).send('The \'Unit Type\' is required!');
+            res.status(400).send('The \'Unit Type\' is required');
         } else if (!testType) {
             res.status(400).send('The \'Test Type\' is required');
         } else if (!testLevel) {
@@ -32,6 +33,45 @@ router.get('/new-patriot-auto', (req, res) => {
             res.status(400).send('The \'Number of Questions\' is required');
         }
     }
+});
+
+
+// Route for saving a newly created test
+router.post('/save-new', (req, res) => {
+    // Declare new test
+    let newTest = new Test(req.body);
+    
+    // Validate test requirements before going any further
+    newTest.validate((err) => {
+        if (err) {
+            console.log('Validation Failed: ' + err);
+            res.status(400).send(err);
+        } else {
+            // Save new Question document to the database
+            newTest.save()
+                .then((result) => {
+                    console.log(result);
+                    res.send('The test has been successfully saved!');
+                })
+                .catch((err) => {
+                    console.log('Error: ' + err);
+                    res.status(500).send(err);
+                });
+        }
+    });
+});
+
+// Route for receiving all tests created by the user that is currently logged in
+router.get('/user-tests', (req, res) => {    
+    if (req.user) {
+        Test.find({created_by: req.user}).populate('versions.questions.question')
+            .then(doc => res.send(doc))
+            .catch(err => res.status(400).send(err));
+    } else {
+        res.status(400).send('The \'User\' is required. Please log into ' +
+            'the Test Vortex Web App to create a new sesstion.');
+    }
+
 });
 
 module.exports = router;
