385a71b14b1ea905a00eb39f5bcd79b84190194d
Kyle <chiefmccain85@gmail.com>
2020-07-31 13:48:42 -0600

    Added 'Save Test' modal

--- client/src/App/Components/Tests/Components/NewTest/Components/TestGenWizard/Components/GenerateTest/GenerateTest.js
+++ client/src/App/Components/Tests/Components/NewTest/Components/TestGenWizard/Components/GenerateTest/GenerateTest.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import TestVersionCard from './Components/TestVersionCard/TestVersionCard';
 import NavigationButtons from '../shared-components/NavigationButtons';
+import TestSaveModal from './Components/TestSaveModal/TestSaveModal';
 
 import pdfMake from 'pdfmake/build/pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
@@ -15,27 +16,50 @@ class GenerateTest extends React.Component {
         super(props);
 
         this.state = {
-            date: this.getTodaysDateString()
+            testSaveModal: false,
+            testName: ''
         };
     }
-    
-    // Creates a string of the current date in the DDMMMYY format
-    getTodaysDateString = () => {
-        const today = new Date(Date.now());
-        return `${today.getDate()}${new Intl.DateTimeFormat('en-US', {month: 'short'}).format(today).toUpperCase()}${today.getYear()-100}`;
+
+    handleInputChange = (event) => {
+        const {target: { id, value}} = event;
+
+        this.setState({[id]: value});   
+    };
+
+    handleClickEvent = (event) => {
+        event.preventDefault();
+
+        if (event.target.id === 'saveTestButton') {
+            this.toggleSaveTestModal();
+        } else if (event.target.id === 'submitTestButton') {
+            this.submitTest();
+        } else if (event.target.id === 'defaultTestNameButton') {
+            this.setDefaultTestName();
+        } else {
+            this.props.clickHandler(event);
+        }
+    }
+
+    toggleSaveTestModal = () => {
+        if (this.state.testSaveModal) {
+            this.setState({testName: '', testSaveModal: false})
+        } else {
+            this.setState({testSaveModal: true});
+        }
     }
 
     // Create document definition for the test
     createTestDefinition = (version) => {
         return PatriotTestFormat(this.props.unitType, this.props.testLevel, 
-            this.props.testType, version, this.state.date, this.props.testQuestions,
+            this.props.testType, version, this.props.date, this.props.testQuestions,
             this.props.testVersions.find((testOutline) => (testOutline.version === version)));
     }
 
     // Create document definition for the answer key
     createAnswerKeyDefinition = (version) => {
         return PatriotKeyFormat(this.props.unitType, this.props.testLevel, 
-            this.props.testType, version, this.state.date, this.props.testQuestions,
+            this.props.testType, version, this.props.date, this.props.testQuestions,
             this.props.testVersions.find((testOutline) => (testOutline.version === version)));
     }
 
@@ -75,6 +99,23 @@ class GenerateTest extends React.Component {
         pdfMake.createPdf(this.createAnswerKeyDefinition(version)).download();
     }
 
+    setDefaultTestName = () => {
+        let testName = `${this.props.unitType} ${this.props.testLevel} ${this.props.testType} Test - ${this.props.date}`;
+        this.setState({testName});
+    }
+
+    // Closes the test save modal and submits the completed test to the server
+    submitTest = () => {
+        this.setState({testSaveModal: false}, () => {
+            let newTest = {
+                test_name: this.state.testName,
+                versions: this.props.testVersions
+            }
+
+            this.props.saveNewTest(newTest);
+        })
+    }
+
     render() {
         const testCards = this.props.testVersions.map((testOutline) => {
             return (<TestVersionCard version={testOutline.version} key={`Version ${testOutline.version}`}
@@ -90,9 +131,13 @@ class GenerateTest extends React.Component {
                 <h2 className='pb-4'>Step 5: Preview/Download a Test or Save for Later</h2>
                 {testCards}
                 <NavigationButtons  previousButton={true} nextButton={true} 
-                    nextButtonID='submitButton' nextButtonText='Save Test'
+                    nextButtonID='saveTestButton' nextButtonText='Save Test'
                     isNextButtonDisabled={false}
-                    clickHandler={this.props.clickHandler}/>
+                    clickHandler={(event) => this.handleClickEvent(event)}/>
+                <TestSaveModal modal={this.state.testSaveModal} toggle={this.toggleSaveTestModal}
+                    testName={this.state.testName}
+                    inputChange={(event) => this.handleInputChange(event)}
+                    clickHandler={(event) => this.handleClickEvent(event)}/>
             </div> 
         );
     }
