ad4e0b0c9c1ce9207b384998f7109f56931ae9b5
Kyle <chiefmccain85@gmail.com>
2020-08-25 20:48:36 -0600

    Refactored Issues API router for external modules

--- server/routes/api/issues/index.js
+++ server/routes/api/issues/index.js
@@ -7,38 +7,10 @@
 // Create primary router for the all 'Issues' API routes
 const IssuesAPIRouter = require('express').Router();
 
-// 
-const Issue = require('../../../models/issue');
-
 IssuesAPIRouter.post('/submit', require('./submit'));
-
-IssuesAPIRouter.get('/get', (req, res) => {
-    Issue.find({issue_number: req.body.issueNumber})
-        .then((results) => {
-            res.send(results);
-        }).catch((err) => {
-            console.log('Error: ' + err);
-            res.status(404).send(err);
-        });
-});
-
-IssuesAPIRouter.get('/getAll', (req, res) => {
-    Issue.find().then((results) => {
-        res.send(results);
-    }).catch((err) => {
-        console.log('Error: ' + err);
-        res.status(404).send(err);
-    });
-});
-
-// router.post('/delete', (req, res) => {
-//     Issue.find.then((results) => {
-//         res.send(results);
-//     }).catch((err) => {
-//         console.log('Error: ' + err);
-//         res.status(404).send(err);
-//     });
-// });
+IssuesAPIRouter.get('/byID/:id', require('./retrieve-by-id'));
+IssuesAPIRouter.get('/all', require('./retrieve-all'));
+IssuesAPIRouter.delete('/byID/:id', require('./delete'));
 
 // Export Issues API router
 module.exports = IssuesAPIRouter;
