5ccf100557bd710b72ddeacf8d0a178f4aa54df3
Kyle <chiefmccain85@gmail.com>
2021-02-02 16:07:02 -0700

    Added query header and query list

--- client/src/App/Components/Issues/Components/ViewIssues/ViewIssues.js
+++ client/src/App/Components/Issues/Components/ViewIssues/ViewIssues.js
@@ -1,7 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
+import axios from 'axios';
 
-export default class ViewIssues extends Component {
-    render() {
-        return <div></div>;
-    }
-}
+import BodyCard from '../../../shared-components/BodyCard/BodyCard';
+import SubmissionBody from '../../../shared-components/SubmissionBody/SubmissionBody';
+import QueryAndView from '../../../shared-components/QueryAndView/QueryAndView';
+import QueryAllHeader from '../../../shared-components/QueryAndView/Components/QueryAllHeader/QueryAllHeader';
+import QueryList from '../../../shared-components/QueryAndView/Components/QueryList/QueryList';
+import IssueEntry from '../../../shared-components/QueryAndView/Components/QueryList/Components/IssueEntry/IssueEntry';
+
+const ViewIssues = () => {
+    return (
+        <BodyCard title='View Existing Issues'>
+            <SubmissionBody submit={() => {}}>
+                <QueryAndView query={axios.get} queryPath={'/api/issues/all'}>
+                    <QueryAllHeader />
+                    <QueryList entryType='Issue'>
+                        <IssueEntry />
+                    </QueryList>
+                </QueryAndView>
+            </SubmissionBody>
+        </BodyCard>
+    );
+};
+
+export default ViewIssues;
--- client/src/App/Components/Issues/Components/ViewIssues/ViewIssues.test.js
+++ client/src/App/Components/Issues/Components/ViewIssues/ViewIssues.test.js
@@ -1,11 +1,85 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ViewIssues from './ViewIssues';
+import axios from 'axios';
+
+jest.mock('axios');
 
 describe('ViewIssues', () => {
     const wrapper = shallow(<ViewIssues />);
 
     it('renders correctly', () => {
         expect(wrapper).toMatchSnapshot();
+    });
+
+    it('renders the BodyCard component', () => {
+        expect(wrapper.find('BodyCard').exists()).toBe(true);
+    });
+
+    it('sets the title of the card to the correct text', () => {
+        expect(wrapper.find('BodyCard').prop('title')).toEqual('View Existing Issues');
+    });
+
+    it('passes the SubmissionBody component to the BodyCard component', () => {
+        expect(wrapper.find('BodyCard').children().name()).toEqual('SubmissionBody');
+    });
+
+    it('renders the SubmissionBody component', () => {
+        expect(wrapper.find('SubmissionBody').exists()).toBe(true);
+    });
+
+    it('passes the submit function to the SubmissionBody component', () => {
+        expect(wrapper.find('SubmissionBody').prop('submit')).toBeInstanceOf(Function);
+    });
+
+    it('passes the QueryAndView component to SubmissionBody component', () => {
+        expect(wrapper.find('SubmissionBody').children().name()).toEqual('QueryAndView');
+    });
+
+    it('renders the QueryAndView component', () => {
+        expect(wrapper.find('QueryAndView').exists()).toBe(true);
+    });
+
+    it('passes the QueryAllHeader component to QueryAndView component', () => {
+        expect(wrapper.find('QueryAndView').childAt(0).name()).toEqual('QueryAllHeader');
+    });
+
+    it('passes the QueryList component to QueryAndView component', () => {
+        expect(wrapper.find('QueryAndView').childAt(1).name()).toEqual('QueryList');
+    });
+
+    it('passes the query function to the QueryAndView component', () => {
+        expect(wrapper.find('QueryAndView').prop('query')).toBeInstanceOf(Function);
+    });
+
+    it('passes the correct query path to the QueryAndView component', () => {
+        expect(wrapper.find('QueryAndView').prop('queryPath')).toEqual('/api/issues/all');
+    });
+
+    it('passes the entry type to the QueryList component', () => {
+        expect(wrapper.find('QueryList').prop('entryType')).toEqual('Issue');
+    });
+
+    it('passes the IssueEntry component to the QueryList component', () => {
+        expect(wrapper.find('QueryList').children().name()).toEqual('IssueEntry');
+    });
+
+    describe('when the query function is called by QueryAndView', () => {
+        beforeAll(() => {
+            axios.mockImplementation(() => {
+                return {
+                    get: () => {
+                        return Promise.resolve();
+                    }
+                };
+            });
+
+            const query = wrapper.find('QueryAndView').prop('query');
+            query();
+        });
+
+        it('calls the axios get function ', () => {
+            expect(axios.get).toHaveBeenCalled();
+        });
     });
 });
