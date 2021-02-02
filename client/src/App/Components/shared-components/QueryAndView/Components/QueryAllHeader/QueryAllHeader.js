e29f66c87fefba563031abd559a37b6095a795c5
Kyle <chiefmccain85@gmail.com>
2021-02-02 16:00:19 -0700

    Added padding to the bottom of component

--- client/src/App/Components/shared-components/QueryAndView/Components/QueryAllHeader/QueryAllHeader.js
+++ client/src/App/Components/shared-components/QueryAndView/Components/QueryAllHeader/QueryAllHeader.js
@@ -4,7 +4,7 @@ import QueryButton from './Components/QueryButton';
 const QueryAllHeader = (props) => {
     const { hasQueryRan, query } = props;
 
-    return <div>
+    return <div className="pb-4">
         <QueryButton hasQueryRan={hasQueryRan} query={query}/>
     </div>;
 };
