2c6b3b92953392e4c20b777940d1fabfafecffee
Kyle <chiefmccain85@gmail.com>
2021-02-02 15:53:03 -0700

    Added local variable assignment of children props

--- client/src/App/Components/shared-components/SubmissionBody/SubmissionBody.js
+++ client/src/App/Components/shared-components/SubmissionBody/SubmissionBody.js
@@ -4,6 +4,7 @@ import SuccessMessage from '../SuccessMessage/SuccessMessage';
 export default class SubmissionBody extends Component {
     constructor(props) {
         super(props);
+        this.FormView = this.props.children;
 
         this.initialState = {
             submissionResponse: '',
@@ -20,7 +21,7 @@ export default class SubmissionBody extends Component {
     }
 
     getOverallView = () => {
-        if(this.state.successFlag && this.state.submissionResponse != '') {
+        if(this.state.successFlag && this.state.submissionResponse !== '') {
             return this.getSuccessView();
         } else {
             return this.getFormView();
@@ -28,7 +29,7 @@ export default class SubmissionBody extends Component {
     }
 
     getFormView = () => {
-        return React.cloneElement(this.props.children,
+        return React.cloneElement(this.FormView,
             { 
                 submissionResponse: this.state.submissionResponse,
                 submit: (e, data) => this.handleSubmitClick(e, data)
@@ -45,17 +46,17 @@ export default class SubmissionBody extends Component {
         );
     }
 
+    handleSubmitClick = (e, data) => {
+        e.preventDefault();
+
+        this.props.submit(data)
+            .then(response => this.setState({ submissionResponse: response, successFlag: true }))
+            .catch(error => this.setState({ submissionResponse: error }));
+    }
+
     handleReturnClick = (e) => {
         e.preventDefault();
 
         this.setState(this.initialState);
-    }
-
-    handleSubmitClick = (e, data) => {
-        e.preventDefault();
-        
-        this.props.submit(data)
-            .then(response => this.setState({ submissionResponse: response, successFlag: true }))
-            .catch(error => this.setState({ submissionResponse: error }));
     }
 }
