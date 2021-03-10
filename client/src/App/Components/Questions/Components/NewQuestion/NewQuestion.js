9fb73537c5f61bee0522f80a40cf33a8e9b4635f
Kyle <chiefmccain85@gmail.com>
2021-03-10 09:22:46 -0700

    Updating module to use SubmissionBody format

--- client/src/App/Components/Questions/Components/NewQuestion/NewQuestion.js
+++ client/src/App/Components/Questions/Components/NewQuestion/NewQuestion.js
@@ -1,69 +1,25 @@
 import React from 'react';
-import Axios from 'axios';
 
-import QuestionForm from '../QuestionForm/QuestionForm';
-import IBCSQuestionForm from '../QuestionForm/IBCSQuestionForm';
-import SuccessMessage from '../../../shared-components/SuccessMessage/SuccessMessage';
+import SubmissionBody from '../../../shared-components/SubmissionBody/SubmissionBody';
+import PatriotQuestionForm from '../QuestionForm/QuestionForm';
+import IBCSQuestionForm from '../QuestionForm/IBCSQuestionForm/IBCSQuestionForm';
 
-class NewQuestion extends React.Component {
-    constructor(props) {
-        super(props);
+const NewQuestion = (props) => {
+    const { questionType } = props;
+    const QuestionForm = getApplicableQuestionForm(questionType);
 
-        this.initialState = {
-            submissionResponse: '',
-            successAlert: false
-        };
+    return (
+        <SubmissionBody>
+            <QuestionForm title={`Add New Question (${questionType})`}/>
+        </SubmissionBody>
+    );
+};
 
-        this.state = this.initialState;
-    }
-
-    submitNewQuestion = (questionData) => {
-        Axios.post(`/api/questions/${this.props.questionType.toLowerCase()}/pending`, questionData)
-            .then((response) => this.setState({submissionResponse: response, successAlert: true}))
-            .catch((response) => this.setState({submissionResponse: response}));
-    }
-
-    returnToForm = () => {
-        this.setState({submissionResponse: '', successAlert: false});
-    }
-
-    render() {
-        let newQuestionView;
-
-        if (this.state.successAlert) {
-            newQuestionView = (
-                <SuccessMessage message={this.state.submissionResponse.data}
-                    clickHandler={() => this.returnToForm()}/>
-            );
-        } else {
-            newQuestionView = this.getApplicableQuestionForm();
-        }
-        return(
-            <div>
-                {newQuestionView}
-            </div>
-        );
-    }
-
-    getApplicableQuestionForm = () => {
-        if (this.props.questionType === 'Patriot') {
-            return this.getPatriotQuestionForm();
-        } else if (this.props.questionType === 'IBCS') {
-            return this.getIBCSQuestionForm();
-        }
-    }
-
-    getPatriotQuestionForm = () => {
-        return <QuestionForm title={`Add New Question (Patriot)`}
-            submitEvent={(questionData) => this.submitNewQuestion(questionData)}
-            submitButtonText='Submit' cancelButtonText='Clear All'/>
-    }
-
-    getIBCSQuestionForm = () => {
-        return <IBCSQuestionForm title={`Add New Question (IBCS)`}
-            submitEvent={(questionData) => this.submitNewQuestion(questionData)}
-            submitButtonText='Submit' cancelButtonText='Clear All'/>
-    }
-}
+const getApplicableQuestionForm = (questionType) => {
+    if (questionType === 'Patriot')
+        return PatriotQuestionForm;
+    else
+        return IBCSQuestionForm;
+};
 
 export default NewQuestion;
