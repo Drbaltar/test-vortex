cf75fe037b155d0c6208d30b041b5264d744be80
Drbaltar <chiefmccain85@gmail.com>
2021-03-11 07:56:25 -0700

    Added submitMapping prop to SubmissionBody

--- client/src/App/Components/Questions/Components/NewQuestion/NewQuestion.js
+++ client/src/App/Components/Questions/Components/NewQuestion/NewQuestion.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import Axios from 'axios';
 
 import SubmissionBody from '../../../shared-components/SubmissionBody/SubmissionBody';
 import PatriotQuestionForm from '../QuestionForm/QuestionForm';
 import IBCSQuestionForm from '../QuestionForm/IBCSQuestionForm/IBCSQuestionForm';
+import FormButtons from '../../../shared-components/FormButtons';
 
 const NewQuestion = (props) => {
     const { questionType } = props;
     const QuestionForm = getApplicableQuestionForm(questionType);
 
     return (
-        <SubmissionBody>
-            <QuestionForm title={`Add New Question (${questionType})`}/>
+        <SubmissionBody submitMapping={getSubmitMapping(questionType)}>
+            <QuestionForm title={`Add New Question (${questionType})`}>
+                <FormButtons submit={true}/>
+            </QuestionForm>
         </SubmissionBody>
     );
 };
@@ -20,6 +24,15 @@ const getApplicableQuestionForm = (questionType) => {
         return PatriotQuestionForm;
     else
         return IBCSQuestionForm;
+};
+
+const getSubmitMapping = (questionType) => {
+    return {
+        submitButton: {
+            requestFunction: Axios.post,
+            requestURI: `/api/questions/${questionType.toLowerCase()}/pending`
+        }
+    };
 };
 
 export default NewQuestion;
--- client/src/App/Components/Questions/Components/NewQuestion/NewQuestion.test.js
+++ client/src/App/Components/Questions/Components/NewQuestion/NewQuestion.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Axios from 'axios';
 import { shallow } from 'enzyme';
 import NewQuestion from './NewQuestion';
 
 describe('NewQuestion', () => {
-    const wrapper = shallow(<NewQuestion />);
+    const wrapper = shallow(<NewQuestion questionType='Patriot'/>);
 
     it('renders correctly', () => {
         expect(wrapper).toMatchSnapshot();
@@ -25,6 +26,17 @@ describe('NewQuestion', () => {
         it('passes the correct title to the question form', () => {
             expect(wrapper.find('PatriotQuestionForm').prop('title')).toBe('Add New Question (Patriot)');
         });
+
+        it('passes the correct submitMapping object to the SubmissionBody component', () => {
+            const expectedSubmitMapping = {
+                submitButton: {
+                    requestFunction: Axios.post,
+                    requestURI: '/api/questions/patriot/pending'
+                }
+            };
+
+            expect(wrapper.find('SubmissionBody').prop('submitMapping')).toEqual(expectedSubmitMapping);
+        });
     });
 
     describe('when the questionType prop is IBCS', () => {
@@ -39,5 +51,24 @@ describe('NewQuestion', () => {
         it('passes the correct title to the question form', () => {
             expect(wrapper.find('IBCSQuestionForm').prop('title')).toBe('Add New Question (IBCS)');
         });
+
+        it('passes the correct submitMapping object to the SubmissionBody component', () => {
+            const expectedSubmitMapping = {
+                submitButton: {
+                    requestFunction: Axios.post,
+                    requestURI: '/api/questions/ibcs/pending'
+                }
+            };
+
+            expect(wrapper.find('SubmissionBody').prop('submitMapping')).toEqual(expectedSubmitMapping);
+        });
+    });
+
+    it('renders the FormButtons component', () => {
+        expect(wrapper.find('FormButtons').exists()).toBe(true);
+    });
+
+    it('passes the correct props to the FormButtons component', () => {
+        expect(wrapper.find('FormButtons').prop('submit')).toEqual(true);
     });
 });
