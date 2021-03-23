c128ef8b4a3d9a2aba0e8783466da9dc818d33a9
Kyle <chiefmccain85@gmail.com>
2021-03-23 11:22:36 -0600

    Added support to display error message

--- client/src/App/Components/shared-components/CheckBoxGroup.js
+++ client/src/App/Components/shared-components/CheckBoxGroup.js
@@ -1,21 +1,38 @@
 import React from 'react';
 
 const CheckBoxGroup = (props) => {
+    const { heading, boxGroup, checkboxChange, isValid, errorMessage } = props;
+
     return( 
-        <div className="border border-muted rounded mt-3 p-3">
-            {props.boxGroup ? getCheckBoxes(props) : null}
+        <div>
+            <div className={`border border-muted ${getBorderColor(isValid)} rounded p-3`}>
+                {heading ? <p>{heading}</p> : null}
+                {boxGroup ? getCheckBoxes(boxGroup, checkboxChange) : null}
+            </div>
+            {isValid === false ? getErrorMessage(errorMessage) : null}
         </div>
     );
 };
 
-const getCheckBoxes = (props) => {
-    return props.boxGroup.map((entry) => {
-        return <div className="form-check">
-            <input className="form-check-input" type="checkbox" value={entry.value} id="createVersionB"
-                onChange={props.checkboxChange} checked={entry.value}/>
+const getCheckBoxes = (boxGroup, checkboxChange) => {
+    return boxGroup.map((entry) => {
+        return <div className="form-check" key={entry.id}>
+            <input className="form-check-input" type="checkbox" id={entry.id}
+                onChange={checkboxChange} checked={entry.value}/>
             <label className="form-check-label" htmlFor={entry.label}>{entry.label}</label>
-        </div>
-    })
+        </div>;
+    });
+};
+
+const getBorderColor = (isValid) => {
+    if (isValid !== null)
+        return isValid ? 'border-success' : 'border-danger';
+    else
+        return '';
+};
+
+const getErrorMessage = (errorMessage) => {
+    return <small style={{color: 'darkred'}}>{errorMessage}</small>;
 };
 
 export default CheckBoxGroup;
